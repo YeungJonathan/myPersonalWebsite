921ba573d679fb3d13b35b7ee0bfdfe9903cc6da
Jonathan Yeung <jonathanyeung1997@gmail.com>
2019-08-09 13:10:59 +1000

    Change background photo and gitignore

--- src/App.js
+++ src/App.js
@@ -16,13 +16,13 @@ function App() {
 
   return (
     <>
-    <div style={styles.backgroundContainer} className="melbourne-background background-photo">
+    <div style={styles.backgroundContainer} className="hamilton-background background-photo">
       <h1 style={{color:'white', fontSize:50}}>Jonathan Yeung</h1>
     </div>    
-    <div style={styles.backgroundContainer} className="hong-kong-background background-photo">
+    <div style={styles.backgroundContainer} className="sydney-background background-photo">
         <h1 style={{color:'white', fontSize:50}}>Software Engineer Intern @ Vonex Ltd</h1>
     </div>
-    <div style={styles.backgroundContainer} className="newyork-background background-photo">
+    <div style={styles.backgroundContainer} className="great-ocean-road-background background-photo">
         <h1 style={{color:'white', fontSize:50}}>Final Year Monash Uni Bachelor of Comp Science</h1>
     </div>
     </>
