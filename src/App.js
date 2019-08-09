76fb7e4d24e336a0e589290f102a00d27c4320ec
Jonathan Yeung <jonathanyeung1997@gmail.com>
2019-08-09 13:29:20 +1000

    Refactor code

--- src/App.js
+++ src/App.js
@@ -1,32 +1,16 @@
 import React from 'react';
-import './App.css';
-
-
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import mainpage from './MainPage'
 
 function App() {
-  
-  const styles={
-    backgroundContainer:{
-      display: 'flex',  
-      justifyContent:'center', 
-      alignItems:'center', 
-      height: '100vh'
-    }
-  }
 
   return (
-    <>
-    <div style={styles.backgroundContainer} className="hamilton-background background-photo">
-      <h1 style={{color:'white', fontSize:50}}>Jonathan Yeung</h1>
-    </div>    
-    <div style={styles.backgroundContainer} className="sydney-background background-photo">
-        <h1 style={{color:'white', fontSize:50}}>Software Engineer Intern @ Vonex Ltd</h1>
-    </div>
-    <div style={styles.backgroundContainer} className="great-ocean-road-background background-photo">
-        <h1 style={{color:'white', fontSize:50}}>Final Year Monash Uni Bachelor of Comp Science</h1>
-    </div>
-    </>
-
+    // <>
+    <Router>
+      <Switch>
+        <Route path="/" component={mainpage} />
+      </Switch>
+    </Router>
   );
 }
 
---
+++ src/MainPage.js
@@ -0,0 +1,38 @@
+import React from 'react';
+import './MainPage.css';
+
+
+function MainPage() {
+  
+  const styles={
+    backgroundContainer:{
+      display: 'flex',  
+      justifyContent:'center', 
+      alignItems:'center', 
+      height: '100vh'
+    },
+    headerText:{
+      color:'white', 
+      fontSize:45,
+      marginLeft: 'auto',
+      marginRight:'auto'
+    }
+  }
+
+  return (
+    <>
+    <div style={styles.backgroundContainer} className="hamilton-background background-photo">
+      <h1 style={styles.headerText}>Jonathan Yeung</h1>
+    </div>    
+    <div style={styles.backgroundContainer} className="sydney-background background-photo">
+        <h1 style={styles.headerText}>Software Engineer Intern @Vonex Ltd</h1>
+    </div>
+    <div style={styles.backgroundContainer} className="great-ocean-road-background background-photo">
+        <h1 style={styles.headerText}>Final Year Monash Uni Bachelor of Comp Science</h1>
+    </div>
+    </>
+
+  );
+}
+
+export default MainPage;
