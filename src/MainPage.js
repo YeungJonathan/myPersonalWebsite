8844d661eb8278a55b8ae0ac8ab3e08f1b09e671
Jonathan Yeung <jonathanyeung1997@gmail.com>
2019-08-12 22:50:43 +1000

    Added self description

--- src/MainPage.js
+++ src/MainPage.js
@@ -6,14 +6,15 @@ function MainPage() {
   
   const styles={
     backgroundContainer:{
+      flexDirection:'column',
       display: 'flex',  
       justifyContent:'center', 
       alignItems:'center', 
       height: '100vh',
     },
     headerText:{
-      color:'white', 
-      fontSize:45,
+      color:'#E0FFFF', 
+      fontSize:80,
       marginLeft: 'auto',
       marginRight:'auto',
     },
@@ -22,18 +23,20 @@ function MainPage() {
   return (
     <>
       <div style={styles.backgroundContainer} className="hamilton-background background-photo">
-        
-        <h1 style={styles.headerText}>Jonathan Yeung</h1>
+          <h1 style={styles.headerText}>Jonathan Yeung</h1>
+          <h3 style={{color:'#E0FFFF'}}> <span style={{color:'#42ecf5', fontSize:32}}>Hong Kong</span> borned, <span style={{color:'#42ecf5', fontSize:32}}>Melbourne</span> based <span style={{color:'#42ecf5', fontSize:32}}>Software Engineer.</span></h3>
+        <Footer/>
       </div>    
 
-      <div style={styles.backgroundContainer} className="sydney-background background-photo">
+      {/* <div style={styles.backgroundContainer} className="sydney-background background-photo">
           <h1 style={ styles.headerText}>Software Engineer Intern @Vonex Ltd</h1>
-      </div>
+      </div> */}
 
-      <div style={styles.backgroundContainer} className="great-ocean-road-background background-photo">
-        <h1 style={styles.headerText}>Final Year Monash Uni Bachelor of Comp Science</h1>
-        <Footer/>
-      </div>
+      {/* <div style={styles.backgroundContainer} className="great-ocean-road-background background-photo"> */}
+      {/* <div style={styles.backgroundContainer}> */}
+        {/* <h1 style={styles.headerText}>Final Year Monash Uni Bachelor of Comp Science</h1> */}
+        {/* <Footer/> */}
+      {/* </div> */}
     </>
 
   );
