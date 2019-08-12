4ddabfecfd32689658eeee59c281b4aad42685d2
Jonathan Yeung <jonathanyeung1997@gmail.com>
2019-08-12 19:07:28 +1000

    Reduce photo size

--- src/MainPage.js
+++ src/MainPage.js
@@ -21,15 +21,15 @@ function MainPage() {
 
   return (
     <>
-    <div style={styles.backgroundContainer} className="hamilton-background background-photo">
-      <h1 style={styles.headerText}>Jonathan Yeung</h1>
-    </div>    
-    <div style={styles.backgroundContainer} className="sydney-background background-photo">
-        <h1 style={styles.headerText}>Software Engineer Intern @Vonex Ltd</h1>
-    </div>
-    <div style={styles.backgroundContainer} className="great-ocean-road-background background-photo">
-        <h1 style={styles.headerText}>Final Year Monash Uni Bachelor of Comp Science</h1>
-    </div>
+      <div style={styles.backgroundContainer} className="hamilton-background background-photo">
+        <h1 style={styles.headerText}>Jonathan Yeung</h1>
+      </div>    
+      <div style={styles.backgroundContainer} className="sydney-background background-photo">
+          <h1 style={styles.headerText}>Software Engineer Intern @Vonex Ltd</h1>
+      </div>
+      <div style={styles.backgroundContainer} className="great-ocean-road-background background-photo">
+          <h1 style={styles.headerText}>Final Year Monash Uni Bachelor of Comp Science</h1>
+      </div>
     </>
 
   );
---
+++ src/NavigationHeader.js
@@ -0,0 +1,15 @@
+import React from 'react';
+// import './MainPage.css';
+
+
+function NavigationHeader() {
+
+  return (
+    <>
+
+    </>
+
+  );
+}
+
+export default NavigationHeader;
