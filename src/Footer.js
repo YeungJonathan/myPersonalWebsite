6934a727ae652a501404693ae1dc4e99265ce7a8
Jonathan Yeung <jonathanyeung1997@gmail.com>
2019-08-12 22:24:22 +1000

    Add footer fb and instagram

--- src/Footer.js
+++ src/Footer.js
@@ -1,16 +1,23 @@
 import React from 'react';
 import { GoMarkGithub } from "react-icons/go";
 import { FiLinkedin } from "react-icons/fi";
+import { FaFacebookF, FaInstagram } from "react-icons/fa";
 import './Footer.css';
 
 function Footer(){
     return(
     <div className="container">
         <a href="https://github.com/YeungJonathan">
-            <GoMarkGithub size={40} className="icon-left icon" color={'lightblue'}/>
+            <GoMarkGithub size={40} className="icon-left-more icon" color={'lightblue'}/>
         </a>
         <a href="https://www.linkedin.com/in/jonathan-yeung-25a37814b/">
-            <FiLinkedin size={40} className="icon" color={'lightblue'}/>
+            <FiLinkedin size={40} className="icon-left icon" color={'lightblue'}/>
+        </a>
+        <a href="https://www.facebook.com/profile.php?id=100011289156941&ref=bookmarks">
+            <FaFacebookF size={40} className="icon-left icon" color={'lightblue'}/>
+        </a>
+        <a href="https://www.instagram.com/jono.y/?hl=en">
+            <FaInstagram size={40} className="icon" color={'lightblue'}/>
         </a>
     </div>
     )
